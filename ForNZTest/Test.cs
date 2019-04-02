fa3578f285f852941679f8dbd676d8c76c7b4c66
mloan2017 <mloan2017@gmail.com>
2019-04-02 10:38:51 +0700

    for nz
    for nz

---
+++ ForNZTest/Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+namespace ForNZTest
+{
+   [TestFixture()]
+   public class Test
+   {
+      [Test()]
+      public void TestCase()
+      {
+      }
+
+		[Test()]
+        public void TestAdd()
+        {
+            Assert.AreEqual(28, 28);
+        }
+
+
+
+        [Test()]
+        public void TestSub()
+        {
+            Assert.AreEqual(28, 20);
+        }
+   }
+}
